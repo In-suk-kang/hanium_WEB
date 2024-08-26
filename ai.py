e15321ec6bed2a6b87cc55c8d1149f33a4dd1511
kis0062195 <kis0062195@gmail.com>
2024-08-26 21:22:18 +0900

    Add html,css

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import requests
 def check_vulnerabilities(filepath):
     with open(filepath, 'rb') as file:
         files = {'file': file}
-        url = 'http://43.203.218.174:5000/predict'
+        url = 'http://3.36.90.112:5000/predict' #AWS 동적 URL로 확인 필요
         response = requests.post(url, files=files)
         
     json_obj = json.loads(response.text)
