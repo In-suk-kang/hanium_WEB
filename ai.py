0fead7442dbc6fb9a6f9a33f7daf6f24541cf4a0
kis0062195 <kis0062195@gmail.com>
2024-08-27 01:03:04 +0900

    Update : Project name -> Code Boom Boom

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import requests
 def check_vulnerabilities(filepath):
     with open(filepath, 'rb') as file:
         files = {'file': file}
-        url = 'http://3.36.90.112:5000/predict' #AWS 동적 URL로 확인 필요
+        url = 'http://52.79.179.141:5000/predict' #AWS 동적 URL로 확인 필요
         response = requests.post(url, files=files)
         
     json_obj = json.loads(response.text)
