e3d7ee552e2bd089023e2f26864ed102feb99811
sbeeno <121777732+sbeeno@users.noreply.github.com>
2024-08-31 00:35:44 +0900

    output

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import requests
 def check_vulnerabilities(filepath):
     with open(filepath, 'rb') as file:
         files = {'file': file}
-        url = 'http://43.202.61.167:5000/predict' #AWS 동적 URL로 확인 필요
+        url = 'http://3.36.114.109:5000/predict' #AWS 동적 URL로 확인 필요
         response = requests.post(url, files=files)
         
     json_obj = json.loads(response.text)
