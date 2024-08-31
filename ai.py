14d98e5968dffd81f67f2f9204caa8052ca602a7
kis0062195 <kis0062195@gmail.com>
2024-08-31 20:55:53 +0900

    Modify : footer css

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import requests
 def check_vulnerabilities(filepath):
     with open(filepath, 'rb') as file:
         files = {'file': file}
-        url = 'http://3.36.114.109:5000/predict' #AWS 동적 URL로 확인 필요
+        url = 'http://13.125.197.144:5000/predict' #AWS 동적 URL로 확인 필요
         response = requests.post(url, files=files)
         
     json_obj = json.loads(response.text)
